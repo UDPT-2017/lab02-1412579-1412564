2a9cdc08be43d789e94c812e4e0781511dd8f663
1412579 - Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-05-09 01:10:15 +0700

    update mailbox frontend

--- route/routes.js
+++ route/routes.js
@@ -33,29 +33,82 @@ module.exports = function(app, passport,pool) {
 	});
 
 	app.get('/', function(req, res) {
-		// pool.connect(function (err) {
-		//   if (err) return console.log(err);
-
-		// 	  // execute a query on our database
-		// 	  pool.query('SELECT * FROM post,users where post.iduser = users.id ORDER BY idpost DESC OFFSET 0 LIMIT 2', function (err, result) {
-		// 	    if (err) {
-		// 	    	res.end();
-		// 	    	return console.log(err);
-		// 	    }
-		// 	    // disconnect the client
-		// 	    res.render('index.ejs',{
-		// 			user : req.user,
-		// 			list : result ,
-		// 			nav: 1
-		// 		}); 
-		// 	});
-
-			
-		// });
-		res.render('index');
+		res.render('index'); // load the index.ejs file
 	});
 
+	app.get('/mailbox', function(req, res) {
+		res.render('mailbox'); // load the index.ejs file
+	});
+
+	app.get('/compose-email', function(req, res) {
+		res.render('compose'); // load the index.ejs file
+	});
+
+	app.get('/read-email', function(req, res) {
+		res.render('reademail'); // load the index.ejs file
+	});
+
+	// =====================================
+	// LOGIN ===============================
+	// =====================================
+	// show the login form
 	app.get('/login', function(req, res) {
+
+		// render the page and pass in any flash data if it exists
+		res.render('login', { message: req.flash('loginMessage') });
+	});
+
+	// process the login form
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/mailbox', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
+	// =====================================
+	// SIGNUP ==============================
+	// =====================================
+	// show the signup form
+	app.get('/signup', function(req, res) {
+
+		// render the page and pass in any flash data if it exists
+		res.render('signup', { message: req.flash('signupMessage') });
+	});
+
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/mailbox', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
+	// =====================================
+	// PROFILE SECTION =========================
+	// =====================================
+	// we will want this protected so you have to be logged in to visit
+	// we will use route middleware to verify this (the isLoggedIn function)
+
+	// =====================================
+	// FACEBOOK ROUTES =====================
+	// =====================================
+	// route for facebook authentication and login
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/mailbox',
+			failureRedirect : '/'
+		}));
+
+	// =====================================
+	// LOGOUT ==============================
+	// =====================================
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
 		// pool.connect(function (err) {
 		//   if (err) return console.log(err);
 
@@ -75,9 +128,7 @@ module.exports = function(app, passport,pool) {
 
 			
 		// });
-		res.render('login');
-	});
-	
+
 };
 
 // route middleware to make sure
